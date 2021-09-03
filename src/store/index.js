47faee24bf46060b8c1ba622b9f0f300863498b0
wangyanwei <740484522@qq.com>
2021-09-03 11:48:21 +0800

    修改配置信息

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,18 @@ import actions from './actions'
 import getters from './getters'
 //1.安装插件
 Vue.use(Vuex)
+
 //2.创建store实例
+const state = {
+  cartList:[]
+}
 const store = new Vuex.Store({
-  state:{
-    cartList:[]
-  },
+  //ES6写法: 键名与键值同名时可简写
+  state,
   mutations,
   actions,
   getters
 })
+
 //3.导出store实例挂载到vue实例上
 export default store;
